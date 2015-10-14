e426b9a33787c45e1c787a0ea1a61ed973746e60
tahirwaseer <tahirwaseer@gmail.com>
2015-10-14 18:05:44 +0500

    angular-sanitize module added

--- config/assets/production.js
+++ config/assets/production.js
@@ -9,6 +9,7 @@ module.exports = {
       ],
       js: [
         'public/lib/angular/angular.js',
+        'public/lib/angular/angular-sanitize.min.js',
         'public/lib/angular-resource/angular-resource.js',
         'public/lib/angular-animate/angular-animate.js',
         'public/lib/angular-ui-router/release/angular-ui-router.js',
